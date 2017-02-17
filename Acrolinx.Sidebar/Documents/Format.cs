93268da1386d41b252268f6277578dc2538180ec
Ralf Kühnlein <ralf.kuehnlein@acrolinx.com>
2017-02-17 15:43:58 +0100

    DEV-10984 added auto format

--- Acrolinx.Sidebar/Documents/Format.cs
+++ Acrolinx.Sidebar/Documents/Format.cs
@@ -10,6 +10,10 @@ namespace Acrolinx.Sdk.Sidebar.Documents
     public enum Format
     {
         XML, Text, HTML, Word_XML,
-        Markdown
+        Markdown,
+        /// <summary>
+        /// Use server-side detection of input format based on file name. 5.2 server is required.
+        /// </summary>
+        Auto
     }
 }
