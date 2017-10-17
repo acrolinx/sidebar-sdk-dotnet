93443ffc07e0e4d9c50124261e33fbfdf5d8ec5d
Paresh Deshmukh <paresh.deshmukh@acrolinx.com>
2017-10-17 15:34:24 +0530

    DEV-14027 Assembly version updated

--- Acrolinx.Sidebar/Properties/AssemblyInfo.cs
+++ Acrolinx.Sidebar/Properties/AssemblyInfo.cs
@@ -35,6 +35,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.1.0.9")]
-[assembly: AssemblyFileVersion("1.1.0.9")]
+[assembly: AssemblyVersion("1.1.0.10")]
+[assembly: AssemblyFileVersion("1.1.0.10")]
 
