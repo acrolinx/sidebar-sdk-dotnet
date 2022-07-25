909f9aab706adba008b35d5d9d17f13801265cf2
Abhijeet Narvekar <abhijeet.narvekar@acrolinx.com>
2022-07-25 15:38:11 +0200

    DEV-27759; fix: Not capitalizing existing Types to maintain compatibility and avoid breaking changes

--- Acrolinx.Sidebar/Documents/Format.cs
+++ Acrolinx.Sidebar/Documents/Format.cs
@@ -5,12 +5,12 @@ namespace Acrolinx.Sdk.Sidebar.Documents
 {
     public enum Format
     {
-        XML, TEXT, HTML, WORD_XML,
-        MARKDOWN, CPP, JAVA,
+        XML, Text, HTML, Word_XML,
+        Markdown, CPP, JAVA,
         PROPERTIES, YAML, JSON,
         /// <summary>
         /// Use server-side detection of input format based on file name. 5.2 server is required.
         /// </summary>
-        AUTO
+        Auto
     }
 }
